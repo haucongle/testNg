8326288c7368e7a9677f9236533e1282a44ae8b4
Hau Le <leconghau.me@gmail.com>
2018-12-08 00:47:40 +0700

    pivot table

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,23 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnInit, Inject, ElementRef, AfterViewInit} from '@angular/core';
 import flatpickr from 'flatpickr';
+
+import 'pivottable/dist/pivot.min.js';
+import 'pivottable/dist/pivot.min.css';
+
+declare var jQuery: any;
 
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.styl']
 })
-export class AppComponent implements OnInit {
+export class AppComponent implements OnInit, AfterViewInit {
 
-    constructor() {
+    el: ElementRef;
+
+    constructor(@Inject(ElementRef)el: ElementRef) {
+        this.el = el;
+
     }
 
     ngOnInit() {
@@ -23,5 +32,45 @@ export class AppComponent implements OnInit {
                 console.log(dateStr);
             }
         });
+    }
+
+    ngAfterViewInit() {
+
+        if (!this.el ||
+            !this.el.nativeElement ||
+            !this.el.nativeElement.children) {
+            console.log('cant build without element');
+            return;
+        }
+
+        const container = this.el.nativeElement;
+        const inst = jQuery(container);
+        const targetElement = inst.find('#output');
+
+        if (!targetElement) {
+            console.log('cant find the pivot element');
+            return;
+        }
+
+        // this helps if you build more than once as it will wipe the dom for that element
+        while (targetElement.firstChild) {
+            targetElement.removeChild(targetElement.firstChild);
+        }
+
+        // here is the magic
+        targetElement.pivotUI([
+                {com: 'com1', time: '2018-12-03T08:00:00+08:00', All: '1', Pickup: '1'},
+                {com: 'com1', time: '2018-12-03T08:15:00+08:00', All: '3', Pickup: '2'},
+                {com: 'com1', time: '2018-12-03T08:30:00+08:00', All: '2', Pickup: '3'},
+                {com: 'com1', time: '2018-12-03T08:45:00+08:00', All: '4', Pickup: '4'},
+                {com: 'com2', time: '2018-12-03T08:00:00+08:00', All: '6', Pickup: '1'},
+                {com: 'com2', time: '2018-12-03T08:15:00+08:00', All: '9', Pickup: '2'},
+                {com: 'com2', time: '2018-12-03T08:30:00+08:00', All: '7', Pickup: '8'},
+                {com: 'com2', time: '2018-12-03T08:45:00+08:00', All: '8', Pickup: '5'},
+            ],
+            {
+                rows: ['com'],
+                cols: ['time']
+            });
     }
 }
