2ecb61a82ae60c9590118caf34ab58ccae956134
Hau Le <leconghau.me@gmail.com>
2018-12-08 00:01:59 +0700

    date time picker

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import flatpickr from 'flatpickr';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.styl']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.styl']
 })
-export class AppComponent {
-  title = 'testNg';
+export class AppComponent implements OnInit {
+
+    constructor() {
+    }
+
+    ngOnInit() {
+        flatpickr('.flatpickr', {
+            enableTime: true,
+            time_24hr: true,
+            altInput: true,
+            altFormat: 'Y-m-d h:i K',
+            dateFormat: 'Y-m-d\\TH:i:S',
+            maxDate: 'today',
+            onChange: (selectedDates, dateStr, instance) => {
+                console.log(dateStr);
+            }
+        });
+    }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,29 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {FormsModule} from '@angular/forms';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {
+    MatButtonModule,
+    MatCheckboxModule,
+} from '@angular/material';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent
+    ],
+    imports: [
+        BrowserModule,
+        FormsModule,
+        AppRoutingModule,
+        BrowserAnimationsModule,
+        MatButtonModule,
+        MatCheckboxModule,
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
